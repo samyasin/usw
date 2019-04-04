ad460238ac905ef58f63d909e35349d9cdcac606
AlaaAlraei <49231352+AlaaAlraei@users.noreply.github.com>
2019-04-04 14:07:36 +0300

    delete album
    edit connection between code and database

--- admin/albums.php
+++ admin/albums.php
@@ -1,27 +1,27 @@
-<?php require '../includes/header.php';?>
-<?php require '../includes/connect_db.php';?>
+<?php require '../includes/header.php'; ?>
+<?php require '../includes/connect_db.php'; ?>
 
- <?php 
-      $query  ="SELECT * FROM album";
-      $result = mysqli_query($con,$query);
-	   echo "<section class='client no-padding-top'>";
-       echo "<div class='container-fluid'>";
-       echo "<div class='row'>";
-        while ($album  = mysqli_fetch_assoc($result)) {
-		echo" 
+<?php
+$query = "select * from album"; //read and display data
+$result = mysqli_query($con, $query);
+echo "<section class='client no-padding-top'>";
+echo "<div class='container-fluid'>";
+echo "<div class='row'>";
+while ($album = mysqli_fetch_assoc($result)) {
+    echo" 
 		<div class= 'col-lg-3'>
 	    <div class='card' style='width: 12rem;margin:20px'>
 		<img src='../images/albums/{$album['album_name']}/{$album['album_cover']}' alt='' height='180' width='192'>
 		<div class='card-body'>
 		<h5 class='card-title'>{$album['album_name']}</h5>
 		<a href='images.php?album_id={$album['album_id']}&album_name={$album['album_name']}' class='btn btn-primary'>Show</a>
-				<a href='' data-toggle='modal' data-target='#myModal' class='btn btn-primary'>Delete</a>
+				<a href = 'delete_album.php?album_id={$album['album_id']}'>Delete</a>
 
         </div>	
 		</div>
 		</div>";
-                
-                  echo "<div id='myModal' class='modal fade' style='display: none;'>
+
+    echo "<div id='myModal' class='modal fade' style='display: none;'>
 	<div class='modal-dialog modal-confirm'>
 		<div class='modal-content'>
 			<div class='modal-header'>
@@ -34,18 +34,18 @@
 				<p class='text-center'>Are you sure !</p>
 			</div>
 			<div class='modal-footer'>
-				<a href='delete_album.php?album_id={$album['album_id']}&album_name={$album['album_name']}' class='btn btn-danger btn-block'  id='ok'>Confirm</a>
+				<a href = 'delete_album.php?album_id={$album['album_id']}'>Delete</a>
                                 <a data-dismiss='modal' class='btn btn-primary'>Cancel</a>
 			</div>
 		</div>
 	</div>
      </div>";
-      }
-    
-     
-     
-       
-      echo "
+}
+
+
+
+
+echo "
 	   <section class='client no-padding-top'>
        <div class='container-fluid'>
        <div class='row'>
@@ -56,13 +56,13 @@
 		</div>
 		</section>
 		";
-        echo "</div>
+echo "</div>
 		      </div>
 		          </section>
 		             ";
-     
 
-	echo '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+
+echo '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -88,19 +88,51 @@
     </div>
   </div>
 </div>';
-     
 
-if (isset($_POST['submit'])){
-    if(!empty($_POST['album_name'])){
-	$album_name = $_POST['album_name'];
-	$query = "INSERT INTO `album` (`album_id`, `album_name`, `album_cover`) VALUES (NULL, '{$album_name}', 'album_cover.jpg')";
-	mysqli_query($con,$query);
-	mkdir("../images/albums/$album_name");
-		echo "<meta http-equiv='refresh' content='0'";
-	}
+
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['album_name'])) {
+        $album_name = $_POST['album_name'];
+        $query = "INSERT INTO `album` (`album_id`, `album_name`, `album_cover`) VALUES (NULL, '{$album_name}', 'album_cover.jpg')";
+        mysqli_query($con, $query);
+        mkdir("../images/albums/$album_name");
+        echo "<meta http-equiv='refresh' content='0'";
+    }
 }
+?>
+<!--
+<div class="row m-t-30">
+    <div class="col-md-12">
+        <!-- DATA TABLE--><!--
+        <div class="table-responsive m-b-40">
+            <table class="table table-borderless table-data3">
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    /*
+                    $query = "select * from album"; //read and display data
+                    $result = mysqli_query($con, $query);
+                    while ($testSet = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                        echo " <td> {$testSet['album_id']}</td>";
 
 
- ?>
- 
-<?php require '../includes/footer.php';?>
+                        echo "<td> <a href = 'delete_album.php?album_id={$testSet['album_id']}'>Delete</a></td>"; //delete
+                        echo "</tr>";
+                    }*/
+                    ?>
+
+
+                </tbody>
+            </table>
+        </div>
+        <!-- END DATA TABLE--><!--
+    </div>
+</div>-->
+
+<?php require '../includes/footer.php'; ?>
--- admin/delete_album.php
+++ admin/delete_album.php
@@ -1,19 +1,29 @@
 <?php require '../includes/connect_db.php';
 ?>
-<?php 
-$album_name = $_GET['album_name'];
-$album_id   = $_GET['album_id'];
-$path       = "../images/albums/$album_name/";
-$query      = "SELECT image_src FROM image WHERE album_id=".$album_id;
-$result       = mysqli_query($con,$query);
-while ($image = mysqli_fetch_assoc($result)){
-	unlink($path.$image['image_src']);
-}
-	$q   = "DELETE FROM album WHERE album_id=".$album_id."";
-	if (mysqli_query($con,$q)){
-      rmdir("../images/albums/$album_name");
-      header('location:albums.php');
-	} else {
-		echo "ERROR :".mysqli_error($con);
-	}
+<?php
+
+$query = "delete from album WHERE album_id={$_GET['album_id']}";
+
+mysqli_query($con,$query);
+
+header('location:albums.php');
+
+
+
+  /*$album_name = $_GET['album_name'];
+  $album_id   = $_GET['album_id'];
+  $path       = "../images/albums/$album_name/";
+  $query      = "SELECT image_src FROM image WHERE album_id=".$album_id;
+  $result       = mysqli_query($con,$query);
+  while ($image = mysqli_fetch_assoc($result)){
+  unlink($path.$image['image_src']);
+  }
+  $q   = "DELETE FROM album WHERE album_id={$_GET['album_id']}";
+  echo $q;  die;
+  if (mysqli_query($con,$q)){
+  rmdir("../images/albums/$album_name");
+  header('location:albums.php');
+  } else {
+  echo "ERROR :".mysqli_error($con);
+  } */
 ?>
