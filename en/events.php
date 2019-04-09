a644d68defde7859f720bd55bbf174a59c96dc94
AlaaAlraei <49231352+AlaaAlraei@users.noreply.github.com>
2019-04-09 11:42:39 +0300

    Update events.php

--- en/events.php
+++ en/events.php
@@ -246,7 +246,7 @@
 							<div class="col-xs-12">
 								<div class="lgx-heading-area">
 									<div class="lgx-heading lgx-heading-white">
-										<h2 class="heading-title"><a href="<?php echo $_SERVER['PHP_SELF'];?>" style="color:white">Upcoming Events</a></h2>
+										<h2 class="heading-title"><a href="<?php echo $_SERVER['PHP_SELF'];?>" style="color:white">Events</a></h2>
 									</div>
 									<ul class="breadcrumb">
 										<li><a href="index.php"><i class="icon-home6"></i>Home</a></li>
