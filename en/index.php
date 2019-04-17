fe43d7514ea1669b24d65dddebb22ac3f114dcfd
AlaaAlraei <49231352+AlaaAlraei@users.noreply.github.com>
2019-04-17 14:16:05 +0300

    Update index.php

--- en/index.php
+++ en/index.php
@@ -207,10 +207,10 @@
                                                                         </li>
                                                                 </ul>
                                                         </li>-->
-                                                            <li><a class="lgx-scroll active" href="#lgx-about">About Us</a></li>
-                                                            <li><a class="lgx-scroll " href="#lgx-courses">Courses</a></li>
+                                                            <li><a class="lgx-scroll active" href="about.php">About Us</a></li>
+                                                            <li><a class="lgx-scroll " href="courses.php">Courses</a></li>
                                                             <li><a class="lgx-scroll" href="gallery.php">Events</a></li>
-                                                            <li><a class="lgx-scroll" href="#lgx-photo-gallery" >Gallery</a></li>
+                                                            <li><a class="lgx-scroll" href="gallery.php" >Gallery</a></li>
                                                             <li><a class="lgx-scroll" href="contact.php">Contact Us</a></li>
                                                             <li class="hidden-xs"><a href="#toggle-search" class="search-animate"><span class="glyphicon glyphicon-search"></span></a></li>
                                                         </ul>
