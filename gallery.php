8d54e46c28d615b6a92930fed435247353405eaf
AlaaAlraei <49231352+AlaaAlraei@users.noreply.github.com>
2019-04-17 14:08:53 +0300

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -67,7 +67,7 @@
 
     </head>
 
-    <body class="page page-template">
+    <body class="page page-template2">
 
         <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
